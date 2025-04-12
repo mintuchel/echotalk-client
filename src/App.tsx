85f4508681c8d5a02a53382c473d5e91b2b2a0e2
mintuchel <max3627@naver.com>
2025-04-13 02:03:19 +0900

    chore: add boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">Hello Tailwind!</h1>
+    </div>
+  );
+}
+
+export default App
