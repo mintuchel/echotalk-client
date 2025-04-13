b2ebc5986b15038c86500615cf93c79d164d8783
mintuchel <max3627@naver.com>
2025-04-13 13:06:49 +0900

    add: layout for auth

---
+++ src/components/auth/layout.tsx
@@ -0,0 +1,11 @@
+export function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+      <div className="flex items-center justify-center h-screen border-2 border-gray-300">
+          {children}
+    </div>
+  );
+}
