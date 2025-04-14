83b1c2fb935b629d77ae27d105158dbc7e65a2d7
mintuchel <max3627@naver.com>
2025-04-14 15:20:59 +0900

    add: new router for chatform

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { LoginForm } from '@/components/auth/LoginForm';
 import { SignUpForm } from '@/components/auth/SignUpForm';
+import { ChatForm } from '@/components/chat/ChatForm';
 
 const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
   {
     path: '/signup',
     element: <SignUpForm />
+  },
+  {
+    path: '/chat',
+    element: <ChatForm />
   }
 ])
 
