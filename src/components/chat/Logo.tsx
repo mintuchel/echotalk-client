a8b581d720ee12913838adf704b664e0d371cc56
mintuchel <max3627@naver.com>
2025-06-17 00:52:45 +0900

    feat: add new logo

--- src/components/chat/Logo.tsx
+++ src/components/chat/Logo.tsx
@@ -5,7 +5,7 @@ export function Logo() {
   return (
     <Link to="http://localhost:8000"className="flex items-center gap-2">
       <img width={40} height={40} src={logo} alt="logo"/>
-      <h1 className="text-2xl font-bold">EchoTalk</h1>
+      <h1 className="text-3xl font-bold">EchoTalk</h1>
     </Link>
   );
 }
