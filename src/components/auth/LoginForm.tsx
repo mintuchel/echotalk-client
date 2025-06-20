3d4234118806c981b06ae9c0605a9a3ebe3a5105
mintuchel <max3627@naver.com>
2025-06-20 16:33:54 +0900

    feat: add logo in loginform

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -6,6 +6,8 @@ import { AuthLayout } from "./layout";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { login } from "@/apis/auth";
+import { Link } from "react-router-dom"
+import logo from "@/assets/logo.png"
 
 export function LoginForm() {
 
@@ -30,35 +32,41 @@ export function LoginForm() {
 
   return (
     <AuthLayout>
-      <FormCard title="로그인" footer={{ label: "아직 계정이 없으신가요?", href: "/signup" }}>
-        <form onSubmit={handleSubmit} className="space-y-6">
-          {/* 이메일 */}
-          <div className="space-y-1">
-            <Label htmlFor="email">이메일</Label>
-            <Input
-              id="email"
-              name="email"
-              type="email"
-              placeholder="example@example.com"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}  
-            />
-          </div>
-          {/* 비밀번호 */}
-          <div className="space-y-1">
-            <Label htmlFor="password">비밀번호</Label>
-            <Input
-              id="password"
-              name="password"
-              type="password"
-              placeholder="********"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}  
-            />
-          </div>
-          <Submit className="w-full">로그인</Submit>
-        </form>
-      </FormCard>
+      <div className="flex flex-col items-center justify-center min-h-screen w-full gap-6">
+        <Link to="http://localhost:8000" className="flex items-center gap-2">
+          <img width={60} height={60} src={logo} alt="logo"/>
+          <h1 className="text-5xl font-bold">EchoTalk</h1>
+        </Link>
+        <FormCard title="로그인" footer={{ label: "아직 계정이 없으신가요?", href: "/signup" }}>
+          <form onSubmit={handleSubmit} className="space-y-6">
+            {/* 이메일 */}
+            <div className="space-y-1">
+              <Label htmlFor="email">이메일</Label>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="example@echoit.co.kr"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            {/* 비밀번호 */}
+            <div className="space-y-1">
+              <Label htmlFor="password">비밀번호</Label>
+              <Input
+                id="password"
+                name="password"
+                type="password"
+                placeholder="********"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <Submit className="w-full">로그인</Submit>
+          </form>
+        </FormCard>
+      </div>
     </AuthLayout>
   );
 }
